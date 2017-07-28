99dcda3ee564bd8b49607e3f943aa5081f3c4bd8
suguru.annaka <suguru.annaka@dena.com>
2017-07-28 18:08:23 +0900

    証明書を追加

--- app/src/main/java/play_store_up_test/android/cayhanecamel/jp/myapplication/MainActivity.java
+++ app/src/main/java/play_store_up_test/android/cayhanecamel/jp/myapplication/MainActivity.java
@@ -5,9 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Hogeee", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
